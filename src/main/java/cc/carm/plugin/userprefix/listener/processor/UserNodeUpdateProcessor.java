ff108d0d1c3e3d7d0d642d818a0f2c4cef92b9b2
CarmJos <KarmunJ@outlook.com>
2022-03-09 04:19:44 +0800

    修复 #9 中提到的后续问题

--- src/main/java/cc/carm/plugin/userprefix/listener/processor/UserNodeUpdateProcessor.java
+++ src/main/java/cc/carm/plugin/userprefix/listener/processor/UserNodeUpdateProcessor.java
@@ -1,10 +1,12 @@
 package cc.carm.plugin.userprefix.listener.processor;
 
+import cc.carm.plugin.userprefix.Main;
 import cc.carm.plugin.userprefix.manager.UserManager;
 import cc.carm.plugin.userprefix.ui.PrefixSelectGUI;
 import net.luckperms.api.event.user.UserDataRecalculateEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class UserNodeUpdateProcessor {
 
@@ -13,9 +15,14 @@ public class UserNodeUpdateProcessor {
         if (player == null) return;
         UserManager.checkPrefix(player, true);
         if (PrefixSelectGUI.openingUsers.contains(player)) {
-            // 玩家权限更新，关闭其GUI，以令其重新打开刷新自己的前缀。
-            player.closeInventory();
-            PrefixSelectGUI.removeOpening(player);
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    // 玩家权限更新，同步关闭其GUI，以令其重新打开刷新自己的前缀。
+                    player.closeInventory();
+                    PrefixSelectGUI.removeOpening(player);
+                }
+            }.runTask(Main.getInstance());
         }
     }
 
